3ffc6c1c56ca54f530f080890382b2f6b57d0719
Shri Agarwal <agashri@hotmail.com>
2018-03-19 21:14:34 -0400

    reverted -double kalivetimeout after each retry

--- dfc/keeper.go
+++ dfc/keeper.go
@@ -156,7 +156,7 @@ func (r *proxykalive) keepalive(err error) (stopped bool) {
 		} else {
 			glog.Infof("Warning: target %s fails keepalive, err: %v", sid, err)
 		}
-		responded, stopped := r.poll(si, url, jsbytes, kalivetimeout)
+		responded, stopped := r.poll(si, url, jsbytes)
 		if stopped {
 			return true
 		}
@@ -176,18 +176,17 @@ func (r *proxykalive) keepalive(err error) (stopped bool) {
 	return false
 }
 
-func (r *proxykalive) poll(si *daemonInfo, url string, jsbytes []byte, lkalivetimeout time.Duration) (responded, stopped bool) {
+func (r *proxykalive) poll(si *daemonInfo, url string, jsbytes []byte) (responded, stopped bool) {
 	poller := time.NewTicker(proxypollival)
 	defer poller.Stop()
 	for i := 0; i < proxypollmaxc; i++ {
 		select {
 		case <-poller.C:
-			_, err, _, status := r.p.call(si, url, http.MethodGet, jsbytes, lkalivetimeout)
+			_, err, _, status := r.p.call(si, url, http.MethodGet, jsbytes, kalivetimeout)
 			if err == nil {
 				return true, false
 			}
 			if IsErrConnectionRefused(err) || status == http.StatusRequestTimeout {
-				lkalivetimeout = 2 * lkalivetimeout
 				continue
 			}
 			glog.Warningf("keepalive: Unexpected status %d, err: %v", status, err)
